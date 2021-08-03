fb9b0c572d98b27efcd268927de4b93b62aebf4b
unknown <arian.angoma@tecsup.edu.pe>
2021-08-02 23:46:56 -0500

    Methods Interface

--- src/interfaces/users.ts
+++ src/interfaces/users.ts
@@ -4,4 +4,8 @@ export default interface IUsers extends Document {
     username: string;
     email: string;
     password: string;
+
+    encryptPassword(password: string): string;
+
+    validatePassword(password: string): boolean;
 }
